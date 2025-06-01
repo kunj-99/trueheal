9995fc46bb684f5571140e102b2b7ab1b63378d9
Kunj Patel <kunjp825@gmail.com>
2025-06-02 00:24:50 +0530

    Splash

---
+++ app/src/main/java/com/infowave/trueheal/Splash.java
@@ -0,0 +1,32 @@
+package com.infowave.trueheal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Handler n = new Handler();
+        n.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(Splash.this, singUp.class);
+                startActivity(in);
+                finish();
+            }
+        },2500);
+
+    }
+}
