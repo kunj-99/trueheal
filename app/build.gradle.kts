6b8a65db29cdca6ffb3af9d28d842f62d78e5b63
Kunj Patel <kunjp825@gmail.com>
2025-06-01 23:06:18 +0530

    Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation ("com.android.volley:volley:1.2.1")
+
+    implementation("com.android.volley:volley:1.2.1")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    implementation ("androidx.cardview:cardview:1.0.0")
+
+
+    implementation("androidx.appcompat:appcompat:1.6.1") // CardView for Material Design card
+    implementation ("com.google.android.material:material:1.4.0")
+
+
+
 }
