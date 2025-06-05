740f71c91a30dd21365b30cfd74f1efd92d213da
Kunj Patel <kunjp825@gmail.com>
2025-06-06 00:12:16 +0530

    drawelayout

--- app/src/main/java/com/infowave/trueheal/MainActivity.java
+++ app/src/main/java/com/infowave/trueheal/MainActivity.java
@@ -1,17 +1,33 @@
 package com.infowave.trueheal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.infowave.trueheal.adapter.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    ImageButton heartIcon;
+    TextView toolbarTitle;
+    Button logoutButton;
     ViewPager vp;
     BottomNavigationView btn;
 
@@ -20,8 +36,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.tb);
+        navigationView = findViewById(R.id.nev_view);
+        heartIcon = findViewById(R.id.heart_icon);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+        logoutButton = navigationView.findViewById(R.id.logout_button);
         vp = findViewById(R.id.vp);
         btn = findViewById(R.id.bottem);
+
+        // Set up the toolbar
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        // Drawer Toggle (Hamburger icon)
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Navigation item selection listener
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // TODO: Handle item selection here as needed
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        // Heart icon click
+        heartIcon.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, WishList.class));
+            Toast.makeText(MainActivity.this, "Heart Icon Clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        // Logout button click
+        logoutButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Logout successful", Toast.LENGTH_SHORT).show();
+            // TODO: Implement actual logout logic
+        });
 
         // Set up the ViewPager with the Adapter
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
--- app/src/main/java/com/infowave/trueheal/fragment/Home.java
+++ app/src/main/java/com/infowave/trueheal/fragment/Home.java
@@ -1,47 +1,29 @@
 package com.infowave.trueheal.fragment;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.navigation.NavigationView;
 import com.infowave.trueheal.DoctorSpecialist;
 import com.infowave.trueheal.R;
-import com.infowave.trueheal.WishList;
 import com.infowave.trueheal.adapter.SliderAdapter;
 import com.infowave.trueheal.adapter.SpecialistAdapter;
 
 public class Home extends Fragment {
 
-    DrawerLayout drawerLayout;
-    Toolbar toolbar;
-    NavigationView navigationView;
-    ActionBarDrawerToggle toggle;
-    ImageButton heartIcon;
-    TextView toolbarTitle, showmor;
-    Button logoutButton;
+    TextView showmor;
     RecyclerView recyclerView, specialist;
     SliderAdapter adapter;
 
@@ -72,96 +54,13 @@ public class Home extends Fragment {
     private Runnable runnable;
     private int scrollPosition = 0;
 
-    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the fragment layout
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Initialize the drawer layout and toolbar
-        drawerLayout = rootView.findViewById(R.id.drawer_layout);
         showmor = rootView.findViewById(R.id.showmor);
-        toolbar = rootView.findViewById(R.id.tb);
-        navigationView = rootView.findViewById(R.id.nev_view);
-        heartIcon = rootView.findViewById(R.id.heart_icon);
-        toolbarTitle = rootView.findViewById(R.id.toolbar_title);
-        logoutButton = rootView.findViewById(R.id.logout_button);
-
-        //Set up the toolbar
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null) {
-            ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
-        }
-
-        // Set up the drawer toggle
-        toggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        // Set up navigation item selection listener
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-
-//                if (id == R.id.payments1) {
-//                    // Handle "Payments" click
-//                    Intent intent = new Intent(getActivity(), payments.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.records1) {
-//                    // Handle "Medical Records" click
-//                    Intent intent = new Intent(getActivity(), medicalrecords.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.policy) {
-//                    // Handle "Privacy Policy" click
-//                    Intent intent = new Intent(getActivity(), privacypolicy.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.notification) {
-//                    // Handle "Notification" click
-//                    Intent intent = new Intent(getActivity(), notification.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.support1) {
-//                    // Handle "Support" click
-//                    Intent intent = new Intent(getActivity(), support.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.shareapp1) {
-//                    // Handle "Share App" click
-//                    Intent intent = new Intent(getActivity(), shareapp.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.aboutus1) {
-//                    // Handle "About Us" click
-//                    Intent intent = new Intent(getActivity(), aboutus.class);
-//                    startActivity(intent);
-//                } else if (id == R.id.settings1) {
-//                    // Handle "Settings" click
-//                    Intent intent = new Intent(getActivity(), settings.class);
-//                    startActivity(intent);
-//                }
-
-                drawerLayout.closeDrawers();
-                return true;
-            }
-        });
-
-        // Handle the heart icon click event
-        heartIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in = new Intent(getActivity(), WishList.class);
-                startActivity(in);
-                Toast.makeText(getActivity(), "Heart Icon Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Handle the logout button click event
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Logout successful", Toast.LENGTH_SHORT).show();
-                // Implement logout logic here
-            }
-        });
 
         // Initialize the main RecyclerView for image slider
         recyclerView = rootView.findViewById(R.id.recyclerView);
@@ -188,11 +87,10 @@ public class Home extends Fragment {
         SpecialistAdapter specialistAdapter = new SpecialistAdapter(specialistImages, specialistNames);
         specialist.setAdapter(specialistAdapter);
 
-       //  Handle the show more button click event
+        //  Handle the show more button click event
         showmor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Replace `YourTargetActivity.class` with the actual target activity
                 startActivity(new Intent(getActivity(), DoctorSpecialist.class));
             }
         });
