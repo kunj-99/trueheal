02afe3b5d0d62b907c1490831fcd69ad6ea62b94
harshil bhudiya <bhudiyaharshil6@gmail.com>
2025-06-10 19:44:35 +0530

    about

--- app/src/main/java/com/infowave/trueheal/AboutUs.java
+++ app/src/main/java/com/infowave/trueheal/AboutUs.java
@@ -1,6 +1,7 @@
 package com.infowave.trueheal;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AboutUs extends AppCompatActivity {
 
+    LinearLayout li;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about_us);
+
+        li = findViewById(R.id.about_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about_page), (v, insets) -> {
+            Insets statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0, statusBarInsets.top, 0, 0);
+            return insets;
+        });
+
 
     }
 }
